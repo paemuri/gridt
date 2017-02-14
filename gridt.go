ea3c4008b8432de144c007a5306456f77320c96e
Rafael Escobar <rescobar@tuta.io>
2017-02-14 13:56:33 -0200

    Handle list with one value and remove returning error.

--- gridt.go
+++ gridt.go
@@ -9,6 +9,17 @@ const (
 	Unlimited uint = 0
 )
 
-func FromBidimensional(values []string, max uint) (widths []int, fit bool, err error) {
-	return []int{}, true, nil
+func FromBidimensional(values []string, max uint) (widths []int, ok bool) {
+	switch len(values) {
+	case 0:
+		return []int{}, true
+	case 1:
+		firstLen := len(values[0])
+		if max == Unlimited || int(max) >= firstLen {
+			return []int{firstLen}, true
+		}
+		return nil, false
+	default:
+		return nil, false
+	}
 }
--- gridt_test.go
+++ gridt_test.go
@@ -11,18 +11,42 @@ const (
 
 func TestFromBidimensional(t *testing.T) {
 	t.Run("With zero items and unlimited size", func(t *testing.T) {
-		ws, f, err := FromBidimensional([]string{}, Unlimited)
-		const msg = "\nShould return empty list of widths that fits, with no errors: "
-		if len(ws) != 0 || !f || err != nil {
-			t.Fatalf("%s%s\nwidths = %v\nfit = %v\nerr = %v", msg, ballotX, ws, f, err)
+		ws, f := FromBidimensional([]string{}, Unlimited)
+		const msg = "\nShould return an empty list of widths that fits: "
+		if len(ws) != 0 || !f {
+			t.Fatalf("%s%s\nwidths = %v\nfit = %v", msg, ballotX, ws, f)
 		}
 		t.Log(msg, checkMark)
 	})
 	t.Run("With zero items and limited size", func(t *testing.T) {
-		ws, f, err := FromBidimensional([]string{}, 10)
-		const msg = "\nShould return empty list of widths that fits, with no errors: "
-		if len(ws) != 0 || !f || err != nil {
-			t.Fatalf("%s%s\nwidths = %v\nfit = %v\nerr = %v", msg, ballotX, ws, f, err)
+		ws, f := FromBidimensional([]string{}, 10)
+		const msg = "\nShould return an empty list of widths that fits: "
+		if len(ws) != 0 || !f {
+			t.Fatalf("%s%s\nwidths = %v\nfit = %v", msg, ballotX, ws, f)
+		}
+		t.Log(msg, checkMark)
+	})
+	t.Run("With one item and unlimited size", func(t *testing.T) {
+		ws, f := FromBidimensional([]string{"1234567890"}, Unlimited)
+		const msg = "\nShould return a list with one width that fits: "
+		if len(ws) != 1 || !f {
+			t.Fatalf("%s%s\nwidths = %v\nfit = %v", msg, ballotX, ws, f)
+		}
+		t.Log(msg, checkMark)
+	})
+	t.Run("With one item and sufficient size", func(t *testing.T) {
+		ws, f := FromBidimensional([]string{"1234567890"}, 20)
+		const msg = "\nShould return a list with one width that fits: "
+		if len(ws) != 1 || !f {
+			t.Fatalf("%s%s\nwidths = %v\nfit = %v", msg, ballotX, ws, f)
+		}
+		t.Log(msg, checkMark)
+	})
+	t.Run("With one item and unsufficient size", func(t *testing.T) {
+		ws, f := FromBidimensional([]string{"1234567890"}, 5)
+		const msg = "\nShould return an empty list that does not fit: "
+		if len(ws) != 0 || f {
+			t.Fatalf("%s%s\nwidths = %v\nfit = %v", msg, ballotX, ws, f)
 		}
 		t.Log(msg, checkMark)
 	})

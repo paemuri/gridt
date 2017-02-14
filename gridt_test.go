d8e440b677d81f9d7b6cdc14f449e28bef044116
Rafael Escobar <rescobar@tuta.io>
2017-02-14 15:45:01 -0200

    Add tests for two items.

--- gridt_test.go
+++ gridt_test.go
@@ -37,4 +37,30 @@ func TestFromBidimensional(t *testing.T) {
 		}
 		t.Log(msg, checkMark)
 	})
+
+	// Two items on the list.
+	t.Run("With two items and sufficient size for both", func(t *testing.T) {
+		ws, f := FromBidimensional([]string{"1234567890", "1234567890"}, 50)
+		const msg = "\nShould return a list with two widths that fits: "
+		if len(ws) != 2 || !f {
+			t.Fatalf("%s%s\nwidths = %v\nfit = %v", msg, ballotX, ws, f)
+		}
+		t.Log(msg, checkMark)
+	})
+	t.Run("With two items and sufficient size for one column", func(t *testing.T) {
+		ws, f := FromBidimensional([]string{"1234567890", "1234567890"}, 15)
+		const msg = "\nShould return a list with one width that fits: "
+		if len(ws) != 1 || !f {
+			t.Fatalf("%s%s\nwidths = %v\nfit = %v", msg, ballotX, ws, f)
+		}
+		t.Log(msg, checkMark)
+	})
+	t.Run("With two items and unsufficient size for any", func(t *testing.T) {
+		ws, f := FromBidimensional([]string{"1234567890", "1234567890"}, 5)
+		const msg = "\nShould return an empty list that does not fit: "
+		if len(ws) != 0 || f {
+			t.Fatalf("%s%s\nwidths = %v\nfit = %v", msg, ballotX, ws, f)
+		}
+		t.Log(msg, checkMark)
+	})
 }

fcaadc36ce38a8c3283f2a5dbf591b4e07bd0386
Rafael Escobar <rescobar@tuta.io>
2017-02-15 13:51:32 -0200

    Fix tests and add more cases.

--- gridt_test.go
+++ gridt_test.go
@@ -13,9 +13,18 @@ const (
 	fatalMsgf = logMsgf + "\ncolumns = %v\nlines = %v\nfit = %v"
 )
 
-var randomLists = [][]string{
-	{"a_value", "another_value", "lots_of_cells", "small_value", "biiiiiiiiiiiig_vaaaaaaaaaalue", "with spaces, it's better to read", "abc", "123", "baby_u_n_me", "bla bla blablablablab", "I see the endings, now", "nothing is broken"},
-}
+var (
+	commonLists = [][]string{
+		{},
+		{"1234567890"},
+		{"1234567890", "1234567890"},
+		{"1234567890", "1234567890", "1234567890"},
+	}
+	randomLists = [][]string{
+		{"a_value", "another_value", "lots_of_cells", "small_value", "biiiiiiiiiiiig_vaaaaaaaaaalue", "with spaces, it's better to read", "abc", "123", "baby_u_n_me", "bla bla blablablablab", "I see the endings, now", "nothing is broken"},
+		{"a", "b", "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"},
+	}
+)
 
 func fmtMsg(len int, f bool) string {
 	var list, fit string
@@ -29,40 +38,53 @@ func fmtMsg(len int, f bool) string {
 	return fmt.Sprintf("Should return a%s that %s", list, fit)
 }
 
-func TestFromBidimensional(t *testing.T) {
+func TestFitIntoWidth(t *testing.T) {
 	for _, c := range []struct {
-		v     []string
-		m     uint
-		d     direction
-		s     string
-		lenWs int
-		l     uint
-		f     bool
+		v []string  // cells' values
+		m uint      // maximum width
+		d direction // direction
+		s string    // separator
+		c int       // columns
+		l uint      // lines
+		f bool      // if it fits
 	}{
 		// Empty lists.
-		{[]string{}, 10, TopToBottom, " ", 0, 0, false},
+		{commonLists[0], 10, TopToBottom, " ", 0, 0, false},
+		{commonLists[0], 10, LeftToRight, " ", 0, 0, false},
 
 		// Lists with one cell.
-		{[]string{"1234567890"}, 20, TopToBottom, " ", 1, 1, true},
-		{[]string{"1234567890"}, 5, TopToBottom, " ", 0, 0, false},
+		{commonLists[1], 20, TopToBottom, " ", 1, 1, true},
+		{commonLists[1], 5, TopToBottom, " ", 0, 0, false},
+		{commonLists[1], 20, LeftToRight, " ", 1, 1, true},
+		{commonLists[1], 5, LeftToRight, " ", 0, 0, false},
 
 		// Lists with two cells.
-		{[]string{"1234567890", "1234567890"}, 30, TopToBottom, " ", 2, 1, true},
-		{[]string{"1234567890", "1234567890"}, 15, TopToBottom, " ", 1, 2, true},
-		{[]string{"1234567890", "1234567890"}, 5, TopToBottom, " ", 0, 0, false},
+		{commonLists[2], 30, TopToBottom, " ", 2, 1, true},
+		{commonLists[2], 15, TopToBottom, " ", 1, 2, true},
+		{commonLists[2], 5, TopToBottom, " ", 0, 0, false},
+		{commonLists[2], 30, LeftToRight, " ", 2, 1, true},
+		{commonLists[2], 15, LeftToRight, " ", 1, 2, true},
+		{commonLists[2], 5, LeftToRight, " ", 0, 0, false},
 
 		// Lists with three cells.
-		{[]string{"1234567890", "1234567890", "1234567890"}, 50, TopToBottom, " ", 3, 1, true},
-		{[]string{"1234567890", "1234567890", "1234567890"}, 30, TopToBottom, " ", 2, 2, true},
-		{[]string{"1234567890", "1234567890", "1234567890"}, 15, TopToBottom, " ", 1, 3, true},
-		{[]string{"1234567890", "1234567890", "1234567890"}, 5, TopToBottom, " ", 0, 0, false},
+		{commonLists[3], 50, TopToBottom, " ", 3, 1, true},
+		{commonLists[3], 30, TopToBottom, " ", 2, 2, true},
+		{commonLists[3], 15, TopToBottom, " ", 1, 3, true},
+		{commonLists[3], 5, TopToBottom, " ", 0, 0, false},
+		{commonLists[3], 50, LeftToRight, " ", 3, 1, true},
+		{commonLists[3], 30, LeftToRight, " ", 2, 2, true},
+		{commonLists[3], 15, LeftToRight, " ", 1, 3, true},
+		{commonLists[3], 5, LeftToRight, " ", 0, 0, false},
 
 		// Random-sized lists.
 		{randomLists[0], 72, TopToBottom, "  ", 3, 4, true},
+		{randomLists[0], 72, LeftToRight, "  ", 2, 6, true},
+		{randomLists[1], 100, TopToBottom, "^-._,^-._,^-._,^-._,^-._,^-._,^", 1, 3, true},
+		{randomLists[1], 100, LeftToRight, "^-._,^-._,^-._,^-._,^-._,^-._,^", 1, 3, true},
 	} {
-		msg := fmtMsg(c.lenWs, c.f)
-		ws, l, f := FromBidimensional(c.v, c.m, c.d, c.s)
-		if len(ws) != c.lenWs || l != c.l || f != c.f {
+		msg := fmtMsg(c.c, c.f)
+		ws, l, f := NewWithSize(c.d, c.s, 0).AddRange(c.v).FitIntoWidth(c.m)
+		if len(ws) != c.c || l != c.l || f != c.f {
 			t.Fatalf(fatalMsgf, msg, ballotX, ws, l, f)
 		}
 		t.Logf(logMsgf, msg, checkMark)

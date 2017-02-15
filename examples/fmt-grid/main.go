e8f709a553d2c791aeb729a7459b429e08bca032
Rafael Escobar <rescobar@tuta.io>
2017-02-15 17:59:30 -0200

    Add examples.

---
+++ examples/fmt-grid/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"fmt"
+
+	"github.com/Nhanderu/gridt"
+	"github.com/Nhanderu/trena"
+)
+
+func main() {
+	fmt.Println()
+	width, _, err := trena.Size()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	grid, ok := gridt.NewWithCells(gridt.TopToBottom, "  ", os.Args[1:]...).FitIntoWidth(uint(width))
+	if !ok {
+		fmt.Println("The arguments does not fit in the terminal width!")
+		os.Exit(1)
+	}
+	fmt.Println(grid.String())
+}

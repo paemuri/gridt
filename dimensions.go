8e899ccdadd084f6aef56bdf18e0d9bd772f072d
Rafael Escobar <rescobar@tuta.io>
2017-02-16 12:48:43 -0200

    Fix Grid formatting.

--- dimensions.go
+++ dimensions.go
@@ -29,20 +29,57 @@ func (d Dimensions) Columns() uint {
 
 // String formats the grid into a string.
 func (d Dimensions) String() string {
-	var buffer bytes.Buffer
-	for i, cell := range d.g.v {
-		switch d.g.d {
-		case TopToBottom:
+	var buf bytes.Buffer
+	switch d.g.d {
+	case TopToBottom:
+		last, counter := 0, -1
+		linesBuf := make([]bytes.Buffer, d.l)
+		for i, cell := range d.g.v {
 			i /= int(d.l)
-		case LeftToRight:
-			i %= len(d.ws)
+			if i != last {
+				last = i
+				counter = 0
+			} else {
+				counter++
+			}
+			linesBuf[counter].WriteString(cell)
+			linesBuf[counter].WriteString(strings.Repeat(" ", int(d.ws[i])-len(cell)))
+			if len(d.ws) != i+1 {
+				linesBuf[counter].WriteString(d.g.sep)
+			}
 		}
-		buffer.WriteString(cell)
-		buffer.WriteString(strings.Repeat(" ", int(d.ws[i])-len(cell)))
-		buffer.WriteString(d.g.sep)
-		if len(d.ws) == i+1 {
-			buffer.WriteString("\n")
+		if counter != int(d.l) {
+			for counter++; counter < int(d.l); counter++ {
+				linesBuf[counter].WriteString(strings.Repeat(" ", int(d.ws[len(d.ws)-1])))
+			}
+		}
+		for i, b := range linesBuf {
+			buf.WriteString(b.String())
+			if i+1 != len(linesBuf) {
+				buf.WriteRune('\n')
+			}
+		}
+	case LeftToRight:
+		var i int
+		var cell string
+		for i, cell = range d.g.v {
+			col := i % len(d.ws)
+			buf.WriteString(cell)
+			buf.WriteString(strings.Repeat(" ", int(d.ws[col])-len(cell)))
+			if i+1 != len(d.g.v) {
+				if len(d.ws) == col+1 {
+					buf.WriteRune('\n')
+				} else {
+					buf.WriteString(d.g.sep)
+				}
+			}
+		}
+		if rest := i % len(d.ws); rest != 0 {
+			for rest++; rest < len(d.ws); rest++ {
+				buf.WriteString(d.g.sep)
+				buf.WriteString(strings.Repeat(" ", int(d.ws[rest])))
+			}
 		}
 	}
-	return buffer.String()
+	return buf.String()
 }

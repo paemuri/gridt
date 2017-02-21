63dd7819006b1a0ba540b129f12a0d9c00be3774
Rafael Escobar <rescobar@tuta.io>
2017-02-21 15:20:10 -0300

    Remove unnecessary code.

--- dimensions.go
+++ dimensions.go
@@ -29,76 +29,18 @@ func (d Dimensions) Columns() uint {
 	return uint(len(d.ws))
 }
 
-// OldString formats the grid into a string.
-func (d Dimensions) OldString() string {
-	cols := len(d.ws)
-	var buf bytes.Buffer
-	switch d.g.d {
-	case TopToBottom:
-		last, counter := 0, -1
-		linesBuf := make([]bytes.Buffer, d.l)
-		for i, cell := range d.g.v {
-			i /= int(d.l)
-			if i != last {
-				last = i
-				counter = 0
-			} else {
-				counter++
-			}
-			linesBuf[counter].WriteString(cell)
-			linesBuf[counter].WriteString(strings.Repeat(" ", int(d.ws[i])-runewidth.StringWidth(cell)))
-			if cols != i+1 {
-				linesBuf[counter].WriteString(d.g.sep)
-			}
-		}
-		if counter != int(d.l) {
-			for counter++; counter < int(d.l); counter++ {
-				linesBuf[counter].WriteString(strings.Repeat(" ", int(d.ws[cols-1])))
-			}
-		}
-		for i, b := range linesBuf {
-			buf.WriteString(b.String())
-			if i+1 != len(linesBuf) {
-				buf.WriteRune('\n')
-			}
-		}
-	case LeftToRight:
-		var i int
-		var cell string
-		for i, cell = range d.g.v {
-			col := i % cols
-			buf.WriteString(cell)
-			buf.WriteString(strings.Repeat(" ", int(d.ws[col])-runewidth.StringWidth(cell)))
-			if i+1 != len(d.g.v) {
-				if cols == col+1 {
-					buf.WriteRune('\n')
-				} else {
-					buf.WriteString(d.g.sep)
-				}
-			}
-		}
-		if rest := i % cols; rest != 0 {
-			for rest++; rest < cols; rest++ {
-				buf.WriteString(d.g.sep)
-				buf.WriteString(strings.Repeat(" ", int(d.ws[rest])))
-			}
-		}
-	}
-	return buf.String()
-}
-
 // String formats the grid into a string.
 func (d Dimensions) String() string {
 	var buf bytes.Buffer
-	c := len(d.ws)
-	for line := 0; line < int(d.l); line++ {
-		for column := 0; column < c; column++ {
+	lines, columns := int(d.l), len(d.ws)
+	for line := 0; line < lines; line++ {
+		for column := 0; column < columns; column++ {
 			var i int
 			switch d.g.d {
 			case LeftToRight:
-				i = line*c + column
+				i = column + line*columns
 			case TopToBottom:
-				i = line + int(d.l)*column
+				i = line + lines*column
 			}
 
 			if i >= len(d.g.v) {

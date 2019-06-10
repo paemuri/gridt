8dc6a3463b1dc0e789d6b1ae2636c0f7f5b2c883
Rafael Escobar <rescobar@redventures.com>
2019-06-10 11:52:49 -0300

    Fix examples.

--- examples/fmt-grid/main.go
+++ examples/fmt-grid/main.go
@@ -5,20 +5,23 @@ import (
 	"os"
 
 	"github.com/Nhanderu/gridt"
-	"github.com/Nhanderu/trena"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
+
 	fmt.Println()
-	width, _, err := trena.Size()
+	width, _, err := terminal.GetSize(0)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
+
 	grid, ok := gridt.New(gridt.TopToBottom, "  ", os.Args[1:]...).FitIntoWidth(width)
 	if !ok {
 		fmt.Println("The arguments does not fit in the terminal width!")
 		os.Exit(1)
 	}
+
 	fmt.Println(grid.String())
 }
